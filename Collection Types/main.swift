5d5103db52d99270d237b7112987daaea7c55c26
Temich93s <nil>
2021-09-25 08:16:42 +1000

    Сделано: Синтаксис массивов

--- Collection Types/main.swift
+++ Collection Types/main.swift
@@ -9,3 +9,10 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: Синтаксис массивов
+print("\n//Синтаксис массивов")
+
+var someInt1 = [1, 2, 3, 4, 5]
+let someInt2 = [1, 2, 3, 4, 5]
+var someInt3: Array<Int> = [1, 2, 3, 4, 5]
+print(someInt1, someInt2, someInt3)
