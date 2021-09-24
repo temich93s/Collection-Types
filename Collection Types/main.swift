7d4ea1ffb76a43c5cf49ec04ee177710d05e8146
Temich93s <nil>
2021-09-25 08:26:57 +1000

    Сделано: Создание пустого массива

--- Collection Types/main.swift
+++ Collection Types/main.swift
@@ -16,3 +16,15 @@ var someInt1 = [1, 2, 3, 4, 5]
 let someInt2 = [1, 2, 3, 4, 5]
 var someInt3: Array<Int> = [1, 2, 3, 4, 5]
 print(someInt1, someInt2, someInt3)
+
+
+//MARK: Создание пустого массива
+print("\n//Создание пустого массива")
+
+var someInt4 = [Int]()
+print(someInt4)
+someInt4.append(3)
+print(someInt4)
+someInt4 = []
+print(someInt4)
+
